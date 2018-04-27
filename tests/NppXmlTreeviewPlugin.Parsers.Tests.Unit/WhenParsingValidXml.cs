6426ed0f6d8f63f486a083cff11d5f0791ab4fea
joaoasrosa <joaoasrosa@gmail.com>
2018-04-27 20:28:43 +0200

    Add tests

---
+++ tests/NppXmlTreeviewPlugin.Parsers.Tests.Unit/WhenParsingValidXml.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+using Xunit;
+
+namespace NppXmlTreeviewPlugin.Parsers.Tests.Unit
+{
+    public class WhenParsingValidXml
+    {
+        [Theory]
+        [InlineData(@"./NPP_comments.xml")]
+        [InlineData(@"./NPP_nocomments.xml")]
+        public void GivenValidXml_ThenFoo(string path)
+        {
+            var xml = File.ReadAllText(path);
+
+            var result = NppXmlNode.TryParse(xml, out var nppXmlNode);
+
+            result.Should().BeTrue();
+        }
+    }
+}
