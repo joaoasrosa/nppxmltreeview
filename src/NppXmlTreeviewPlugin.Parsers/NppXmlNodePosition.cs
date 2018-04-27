0b2f3cd3fb32e2fdb47e3a778ba22b63726b2c6c
joaoasrosa <joaoasrosa@gmail.com>
2018-04-27 21:52:35 +0200

    Code cleanup

--- src/NppXmlTreeviewPlugin.Parsers/NppXmlNodePosition.cs
+++ src/NppXmlTreeviewPlugin.Parsers/NppXmlNodePosition.cs
@@ -3,29 +3,29 @@
 namespace NppXmlTreeviewPlugin.Parsers
 {
     /// <summary>
-    /// The npp xml node position.
+    ///     The npp xml node position.
     /// </summary>
     public class NppXmlNodePosition
     {
         /// <summary>
-        /// Internal constructor
+        ///     The line number.
+        /// </summary>
+        public int LineNumber { get; }
+
+        /// <summary>
+        ///     The line position.
+        /// </summary>
+        public int LinePosition { get; }
+
+        /// <summary>
+        ///     Internal constructor
         /// </summary>
         /// <param name="xmlTextReader">The XML text reader.</param>
         /// <param name="isEndPosition">Flag to indicate if it is a end position.</param>
         internal NppXmlNodePosition(XmlTextReader xmlTextReader, bool isEndPosition = false)
         {
-            this.LineNumber = xmlTextReader.LineNumber - 1;
-            this.LinePosition = xmlTextReader.LinePosition + (isEndPosition ? xmlTextReader.Name.Length : -2);
+            LineNumber = xmlTextReader.LineNumber - 1;
+            LinePosition = xmlTextReader.LinePosition + (isEndPosition ? xmlTextReader.Name.Length : -2);
         }
-
-        /// <summary>
-        /// The line number.
-        /// </summary>
-        public int LineNumber { get; private set; }
-
-        /// <summary>
-        /// The line position.
-        /// </summary>
-        public int LinePosition { get; }
     }
 }
